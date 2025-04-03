05e37723e96d9903e57fb68feb1f46176b56c0ba
Hollis McCray <hollis.mccray@gmail.com>
2025-04-03 15:15:02 -0700

    Package

---
+++ workspace/github.com/hollis-mccray/pokedexcli/internal/pokeapi/pokedex-map.go
@@ -0,0 +1,39 @@
+package pokeapi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"encoding/json"
+)
+
+type locationData struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func getMaps(c cliConfig) error {
+	addr := *c.Next
+	res, err := http.Get(addr)
+	if err != nil {
+		return err
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	data := locationData{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return err
+	}
+	for _, location := range data.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
+}
